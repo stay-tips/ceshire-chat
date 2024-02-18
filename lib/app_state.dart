1e147bb1d5cc82522ce2abb5f73bfce61b21a0b4
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-02-18 18:50:40 +0000

    separating messages

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -19,12 +19,6 @@ class FFAppState extends ChangeNotifier {
   Future initializePersistedState() async {
     prefs = await SharedPreferences.getInstance();
     _safeInit(() {
-      _chatServerUri = prefs.getString('ff_chatServerUri') ?? _chatServerUri;
-    });
-    _safeInit(() {
-      _chunks = prefs.getStringList('ff_chunks') ?? _chunks;
-    });
-    _safeInit(() {
       _messages = prefs
               .getStringList('ff_messages')
               ?.map((x) {
@@ -48,52 +42,10 @@ class FFAppState extends ChangeNotifier {
 
   late SharedPreferences prefs;
 
-  String _chatServerUri = 'ws://ha.local/ws';
-  String get chatServerUri => _chatServerUri;
-  set chatServerUri(String value) {
-    _chatServerUri = value;
-    prefs.setString('ff_chatServerUri', value);
-  }
-
   dynamic _socket;
   dynamic get socket => _socket;
   set socket(dynamic value) {
     _socket = value;
-  }
-
-  List<String> _chunks = [];
-  List<String> get chunks => _chunks;
-  set chunks(List<String> value) {
-    _chunks = value;
-    prefs.setStringList('ff_chunks', value);
-  }
-
-  void addToChunks(String value) {
-    _chunks.add(value);
-    prefs.setStringList('ff_chunks', _chunks);
-  }
-
-  void removeFromChunks(String value) {
-    _chunks.remove(value);
-    prefs.setStringList('ff_chunks', _chunks);
-  }
-
-  void removeAtIndexFromChunks(int index) {
-    _chunks.removeAt(index);
-    prefs.setStringList('ff_chunks', _chunks);
-  }
-
-  void updateChunksAtIndex(
-    int index,
-    String Function(String) updateFn,
-  ) {
-    _chunks[index] = updateFn(_chunks[index]);
-    prefs.setStringList('ff_chunks', _chunks);
-  }
-
-  void insertAtIndexInChunks(int index, String value) {
-    _chunks.insert(index, value);
-    prefs.setStringList('ff_chunks', _chunks);
   }
 
   List<MessageStruct> _messages = [];
@@ -135,12 +87,6 @@ class FFAppState extends ChangeNotifier {
     _messages.insert(index, value);
     prefs.setStringList(
         'ff_messages', _messages.map((x) => x.serialize()).toList());
-  }
-
-  dynamic _socket;
-  dynamic get socket => _socket;
-  set socket(dynamic value) {
-    _socket = value;
   }
 }
 
--- lib/backend/schema/structs/index.dart
+++ lib/backend/schema/structs/index.dart
@@ -1,3 +1,5 @@
 export '/backend/schema/util/schema_util.dart';
 
+export 'in_message_struct.dart';
 export 'message_struct.dart';
+export 'out_message_struct.dart';
