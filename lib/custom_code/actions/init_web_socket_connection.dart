661e80ff99d493f43ac2eae07dbe2a579b25b934
Enrico <Miamoto-Musashi@users.noreply.github.com>
2024-02-18 17:29:30 +0100

    dert

--- lib/custom_code/actions/init_web_socket_connection.dart
+++ lib/custom_code/actions/init_web_socket_connection.dart
@@ -21,4 +21,59 @@ Future initWebSocketConnection(
   }
   final uri = endpoint + '/' + userId;
   final socket = WebSocket(Uri.parse(uri));
+
+  socket.messages.listen((message) {
+    // Handle incoming messages.
+    print('*** incoming message $message');
+    //final List<ChatResponseStruct> = convertJsontoChatResp(message);
+    final json = jsonDecode(message);
+    print("*** message decoded ${json['type']}");
+    final res = MessageStruct.fromMap(json);
+
+    print("**** cat error? ${res.error}");
+
+    if (res.type == 'chat') {
+      print("*** chat message ${res.content}");
+      addMessage(res);
+      // addToChatHistory(res, callbackAction);
+      // } else if (res.type == 'property') {
+      //   print("*** property message ${res.content}");
+      //   addToPropertyHistory(res, callbackAction);
+      // } else if (res.type == 'user') {
+      //   print("*** user message ${res.content}");
+      //   addToUserHistory(res, callbackAction);
+    } else if (res.type == 'error') {
+      print("*** error message ${res.content}");
+    } else {
+      print("*** unknown message ${res.content}");
+    }
+
+    //addToChatHistory(json, callbackAction);
+    addMessage(res);
+  });
+
+  // Listen to changes in the connection state.
+  socket.connection.listen((state) {
+    // Handle changes in the connection state.
+    print("**** connection state changed $state");
+  });
+}
+
+void addMessage(MessageStruct message) {
+  if (message.type == 'error') {
+    print("**** cat response has error...");
+  }
+  String? content = message.content;
+
+  // prepare response
+  // final chat = ChatResponseStruct(catResponse: response, text: message);
+  // FFAppState().addToChatHistory(chat);
+
+  FFAppState().updateMessagesAtIndex(
+    FFAppState().messages.length - 1,
+    (e) {
+      e..content = message; //"${e.content}";
+      return e;
+    },
+  );
 }
