ceb19a4d1e06453acd5c5e7d3c1c758d7907b577
Enrico <Miamoto-Musashi@users.noreply.github.com>
2024-02-18 17:50:41 +0100

    Merge remote-tracking branch 'origin/flutterflow'

--- lib/pages/chat/chat_widget.dart
+++ lib/pages/chat/chat_widget.dart
@@ -1,8 +1,9 @@
-import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/custom_code/actions/index.dart' as actions;
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'chat_model.dart';
 export 'chat_model.dart';
@@ -24,6 +25,29 @@ class _ChatWidgetState extends State<ChatWidget> {
     super.initState();
     _model = createModel(context, () => ChatModel());
 
+    // On page load action.
+    SchedulerBinding.instance.addPostFrameCallback((_) async {
+      await actions.initWebSocketConnection(
+        'ws://localhost:1865/ws',
+        'user',
+        () async {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(
+                'connected',
+                style: FlutterFlowTheme.of(context).labelMedium.override(
+                      fontFamily: 'Readex Pro',
+                      color: FlutterFlowTheme.of(context).primaryText,
+                    ),
+              ),
+              duration: const Duration(milliseconds: 4000),
+              backgroundColor: FlutterFlowTheme.of(context).secondary,
+            ),
+          );
+        },
+      );
+    });
+
     _model.textController ??= TextEditingController();
     _model.textFieldFocusNode ??= FocusNode();
   }
@@ -37,6 +61,15 @@ class _ChatWidgetState extends State<ChatWidget> {
 
   @override
   Widget build(BuildContext context) {
+    if (isiOS) {
+      SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(
+          statusBarBrightness: Theme.of(context).brightness,
+          systemStatusBarContrastEnforced: true,
+        ),
+      );
+    }
+
     context.watch<FFAppState>();
 
     return GestureDetector(
@@ -46,197 +79,89 @@ class _ChatWidgetState extends State<ChatWidget> {
       child: Scaffold(
         key: scaffoldKey,
         backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
-        floatingActionButton: Padding(
-          padding: const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 16.0),
-          child: FloatingActionButton(
-            onPressed: () async {
-              if (_model.formKey.currentState == null ||
-                  !_model.formKey.currentState!.validate()) {
-                return;
-              }
-              await actions.sendMessage(
-                _model.textController.text,
-                () async {},
-                () async {
-                  await _model.listViewController?.animateTo(
-                    _model.listViewController!.position.maxScrollExtent,
-                    duration: const Duration(milliseconds: 100),
-                    curve: Curves.ease,
-                  );
-                },
-              );
-              setState(() {});
-              setState(() {
-                _model.textController?.clear();
-              });
-              await Future.delayed(const Duration(milliseconds: 4000));
-              await _model.listViewController?.animateTo(
-                _model.listViewController!.position.maxScrollExtent,
-                duration: const Duration(milliseconds: 100),
-                curve: Curves.ease,
-              );
-            },
-            backgroundColor: FlutterFlowTheme.of(context).primary,
-            elevation: 8.0,
-            child: Icon(
-              Icons.send,
-              color: FlutterFlowTheme.of(context).info,
-              size: 24.0,
-            ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print('FloatingActionButton pressed ...');
+          },
+          backgroundColor: FlutterFlowTheme.of(context).primary,
+          elevation: 8.0,
+          child: Icon(
+            Icons.send,
+            color: FlutterFlowTheme.of(context).info,
+            size: 24.0,
           ),
         ),
         appBar: AppBar(
           backgroundColor: FlutterFlowTheme.of(context).primary,
           automaticallyImplyLeading: false,
-          title: Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                'Chat',
-                style: FlutterFlowTheme.of(context).headlineMedium.override(
-                      fontFamily: 'Outfit',
-                      color: FlutterFlowTheme.of(context).alternate,
-                      fontSize: 22.0,
-                      letterSpacing: 0.0,
-                    ),
-              ),
-              FlutterFlowIconButton(
-                borderColor: FlutterFlowTheme.of(context).primary,
-                borderRadius: 22.0,
-                borderWidth: 1.0,
-                buttonSize: 40.0,
-                fillColor: FlutterFlowTheme.of(context).accent1,
-                icon: Icon(
-                  Icons.settings,
-                  color: FlutterFlowTheme.of(context).alternate,
-                  size: 24.0,
+          title: Text(
+            'Ceshire Chat',
+            style: FlutterFlowTheme.of(context).headlineMedium.override(
+                  fontFamily: 'Outfit',
+                  color: Colors.white,
+                  fontSize: 22.0,
                 ),
-                onPressed: () async {
-                  context.pushNamed('settings');
-                },
-              ),
-            ],
           ),
           actions: const [],
-          centerTitle: true,
+          centerTitle: false,
           elevation: 2.0,
         ),
         body: SafeArea(
           top: true,
-          child: Align(
-            alignment: const AlignmentDirectional(0.0, 1.0),
-            child: SizedBox(
-              height: double.infinity,
-              child: Stack(
-                children: [
-                  Align(
-                    alignment: const AlignmentDirectional(0.0, 1.0),
-                    child: Form(
-                      key: _model.formKey,
-                      autovalidateMode: AutovalidateMode.disabled,
-                      child: Padding(
-                        padding:
-                            const EdgeInsetsDirectional.fromSTEB(8.0, 0.0, 74.0, 0.0),
-                        child: TextFormField(
-                          controller: _model.textController,
-                          focusNode: _model.textFieldFocusNode,
-                          onFieldSubmitted: (_) async {
-                            if (_model.formKey.currentState == null ||
-                                !_model.formKey.currentState!.validate()) {
-                              return;
-                            }
-                            await actions.sendMessage(
-                              _model.textController.text,
-                              () async {
-                                await showDialog(
-                                  context: context,
-                                  builder: (alertDialogContext) {
-                                    return AlertDialog(
-                                      title: const Text('ERROR'),
-                                      content: const Text('need select URI'),
-                                      actions: [
-                                        TextButton(
-                                          onPressed: () =>
-                                              Navigator.pop(alertDialogContext),
-                                          child: const Text('MY BAD'),
-                                        ),
-                                      ],
-                                    );
-                                  },
-                                );
-                              },
-                              () async {
-                                setState(() {});
-                              },
-                            );
-                            setState(() {});
-                          },
-                          autofocus: true,
-                          obscureText: false,
-                          decoration: InputDecoration(
-                            labelText: 'Meaow....',
-                            labelStyle: FlutterFlowTheme.of(context)
-                                .labelMedium
-                                .override(
-                                  fontFamily: 'Readex Pro',
-                                  letterSpacing: 0.0,
-                                ),
-                            hintStyle: FlutterFlowTheme.of(context)
-                                .labelMedium
-                                .override(
-                                  fontFamily: 'Readex Pro',
-                                  letterSpacing: 0.0,
-                                ),
-                            enabledBorder: InputBorder.none,
-                            focusedBorder: InputBorder.none,
-                            errorBorder: InputBorder.none,
-                            focusedErrorBorder: InputBorder.none,
-                          ),
-                          style:
-                              FlutterFlowTheme.of(context).bodyMedium.override(
-                                    fontFamily: 'Readex Pro',
-                                    letterSpacing: 0.0,
-                                  ),
-                          maxLines: null,
-                          validator: _model.textControllerValidator
-                              .asValidator(context),
-                        ),
+          child: SizedBox(
+            height: double.infinity,
+            child: Stack(
+              children: [
+                Align(
+                  alignment: const AlignmentDirectional(0.0, 1.0),
+                  child: Padding(
+                    padding: const EdgeInsetsDirectional.fromSTEB(8.0, 0.0, 8.0, 0.0),
+                    child: TextFormField(
+                      controller: _model.textController,
+                      focusNode: _model.textFieldFocusNode,
+                      autofocus: true,
+                      obscureText: false,
+                      decoration: InputDecoration(
+                        labelText: 'Meaow....',
+                        labelStyle: FlutterFlowTheme.of(context).labelMedium,
+                        hintStyle: FlutterFlowTheme.of(context).labelMedium,
+                        enabledBorder: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        errorBorder: InputBorder.none,
+                        focusedErrorBorder: InputBorder.none,
                       ),
+                      style: FlutterFlowTheme.of(context).bodyMedium,
+                      maxLines: null,
+                      validator:
+                          _model.textControllerValidator.asValidator(context),
                     ),
                   ),
-                  Container(
-                    height: MediaQuery.sizeOf(context).height * 0.79,
-                    decoration: const BoxDecoration(),
-                    child: Builder(
-                      builder: (context) {
-                        final message = FFAppState().messages.toList();
-                        return ListView.builder(
-                          padding: EdgeInsets.zero,
-                          scrollDirection: Axis.vertical,
-                          itemCount: message.length,
-                          itemBuilder: (context, messageIndex) {
-                            final messageItem = message[messageIndex];
-                            return Padding(
-                              padding: const EdgeInsets.all(12.0),
-                              child: Text(
-                                messageItem.content,
-                                style: FlutterFlowTheme.of(context)
-                                    .bodyMedium
-                                    .override(
-                                      fontFamily: 'Readex Pro',
-                                      letterSpacing: 0.0,
-                                    ),
-                              ),
-                            );
-                          },
-                          controller: _model.listViewController,
-                        );
-                      },
-                    ),
+                ),
+                Container(
+                  height: MediaQuery.sizeOf(context).height * 0.79,
+                  decoration: const BoxDecoration(),
+                  child: Builder(
+                    builder: (context) {
+                      final message = FFAppState().messages.toList();
+                      return ListView.builder(
+                        padding: EdgeInsets.zero,
+                        scrollDirection: Axis.vertical,
+                        itemCount: message.length,
+                        itemBuilder: (context, messageIndex) {
+                          final messageItem = message[messageIndex];
+                          return Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Text(
+                              messageItem.hasContent().toString(),
+                              style: FlutterFlowTheme.of(context).bodyMedium,
+                            ),
+                          );
+                        },
+                      );
+                    },
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
