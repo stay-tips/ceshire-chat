2b55bd44810c6a5a28aea11548eaebe99eada71b
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-02-18 16:49:55 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/actions/send_message.dart
+++ lib/custom_code/actions/send_message.dart
@@ -6,7 +6,13 @@ import 'index.dart'; // Imports other custom actions
 import 'package:flutter/material.dart';
 // Begin custom action code
 // DO NOT REMOVE OR MODIFY THE CODE ABOVE!
+import 'dart:convert';
 
 Future sendMessage(String? content) async {
-  FFAppState().socket.send(content);
+  MessageStruct text = MessageStruct(message: content, authorName: 'Human', when: DateTime.now());
+  FFAppState().addToMessages(text);
+  FFAppState().notifyListeners();
+  // send message
+  OutMessageStruct message = OutMessageStruct(text: content);
+  FFAppState().socket.send(jsonEncode(message.toMap()));
 }
